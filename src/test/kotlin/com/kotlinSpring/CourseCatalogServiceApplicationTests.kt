77c6f06af1e04779d1add8747b96db7d78404152
andriusmw <andriusmw@yahoo.es>
2024-01-31 15:31:52 +0100

    initial commit

---
+++ src/test/kotlin/com/kotlinSpring/CourseCatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinSpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CourseCatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
