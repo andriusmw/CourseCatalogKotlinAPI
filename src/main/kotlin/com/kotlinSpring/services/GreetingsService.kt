134fb60137b0585369413b5839c8c4f8686966de
andriusmw <andriusmw@yahoo.es>
2024-02-01 08:34:22 +0100

    accesing application.yml

--- src/main/kotlin/com/kotlinSpring/services/GreetingsService.kt
+++ src/main/kotlin/com/kotlinSpring/services/GreetingsService.kt
@@ -1,9 +1,11 @@
 package com.kotlinSpring.services
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService {
-
-    fun retrieveGreeting(name: String) = "Hello $name"
+    @Value("\${message}") // this access the message on application.yml , idkwbidc
+    lateinit var message: String
+    fun retrieveGreeting(name: String) = "Hello $name, $message"
 }
